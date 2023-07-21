d5310c6d8277bf9362d41d2c17f8967133bf5e57
Anna Smirnova <sm_anna@mail.ru>
2023-07-21 14:45:14 +0300

    Last commit

--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -1,5 +1,6 @@
 
 import { NavLink } from "react-router-dom";
+import React from "react";
 
 const TableRow = (props) => {
 	return (
@@ -14,25 +15,52 @@ const TableRow = (props) => {
 		</tr>
 	);
 };
-
-export const Friends = (props) => {
-	let users = props.function();
-
-	let usersCount = Object.keys(users).length;
-	let userRow = [];
-
-	for (let i = 0; i < usersCount; i++) {
-		userRow.push(
-			<TableRow
-				id={users[i].id}
-				index={i}
-				key={i}
-				name={users[i].name}
-				lastname={users[i].lastname}
-				email={users[i].email}
-			/>
-		);
+export class Friends extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { userRow: [] };
 	}
+
+// export const Friends = (props) => {
+// 	let users = props.function();
+
+// 	let usersCount = Object.keys(users).length;
+// 	let userRow = [];
+
+	// for (let i = 0; i < usersCount; i++) {
+	// 	userRow.push(
+	// 		<TableRow
+	// 			id={users[i].id}
+	// 			index={i}
+	// 			key={i}
+	// 			name={users[i].name}
+	// 			lastname={users[i].lastname}
+	// 			email={users[i].email}
+	// 		/>
+	// 	);
+	// }
+	componentDidMount() {
+		this.props.function().then((users) => {
+			console.log(users);
+			let usersCount = users.length;
+			let userRow = [];
+
+			for (let i = 0; i < usersCount; i++) {
+				userRow.push(
+					<TableRow
+						id={users[i].id}
+						index={i}
+						key={i}
+						name={users[i].name}
+						lastname={users[i].lastname}
+						email={users[i].email}
+					/>
+				);
+			}
+			this.setState({userRow: userRow});
+		});
+	}
+	render() {
 
 	return (
 		<>
@@ -47,10 +75,11 @@ export const Friends = (props) => {
 								<th scope="col">Email</th>
 							</tr>
 						</thead>
-						<tbody>{userRow}</tbody>
+						<tbody>{this.state.userRow}</tbody>
 					</table>
 				</div>
 			</div>
 		</>
 	);
-};
+}
+}
